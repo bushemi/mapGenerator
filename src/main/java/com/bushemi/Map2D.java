21ca841e417bc2274f90f4731009e92f481ab29f
IgorBoldysh <bushemi2001@gmail.com>
2020-02-23 11:30:13 +0200

    add java docs

--- src/main/java/com/bushemi/Map2D.java
+++ src/main/java/com/bushemi/Map2D.java
@@ -2,6 +2,9 @@ package com.bushemi;
 
 import java.util.Random;
 
+/**
+ * For creating or changing map of MapCell objects.
+ */
 public class Map2D {
     private static Random RANDOM = new Random();
     private MapCell[][] map;
@@ -10,6 +13,12 @@ public class Map2D {
     private int maxY;
     private boolean areAllPassable;
 
+    /**
+     * Create empty map.
+     *
+     * @param lengthX maximum X
+     * @param lengthY maximum Y
+     */
     public Map2D(int lengthX, int lengthY) {
         map = new MapCell[lengthX][lengthY];
         mapSize = lengthX * lengthY;
@@ -27,6 +36,13 @@ public class Map2D {
         }
     }
 
+    /**
+     * Get map cell from map.
+     *
+     * @param x int
+     * @param y int
+     * @return MapCell object.
+     */
     public MapCell getMapCell(int x, int y) {
         if ((x >= 0) && (x < maxX) && (y >= 0) && (y < maxY)) {
             return map[x][y];
@@ -34,18 +50,38 @@ public class Map2D {
         throw new RuntimeException("not valid coordinates.");
     }
 
+    /**
+     * Get Map size.
+     *
+     * @return long
+     */
     public long getMapSize() {
         return mapSize;
     }
 
+    /**
+     * Get maximum X for current map.
+     *
+     * @return int maximum X
+     */
     public int getMaxX() {
         return map.length;
     }
 
+    /**
+     * Gget maximum Y for current map.
+     *
+     * @return int maximum Y
+     */
     public int getMaxY() {
         return map[1].length;
     }
 
+    /**
+     * Resets map and generates new obstacles.
+     *
+     * @param percentOfTotalCells int
+     */
     public void regenerateObstacles(int percentOfTotalCells) {
         if (percentOfTotalCells > 0 && percentOfTotalCells < 100) {
             if (!areAllPassable) {
